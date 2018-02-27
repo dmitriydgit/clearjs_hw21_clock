a293f9fab6c71ccca25afe42d8af1c551307382e
dima <dmitriy.dubchenko@gmail.com>
2018-02-27 02:29:04 +0200

    commit + extra stiles

--- js/main.js
+++ js/main.js
@@ -25,31 +25,29 @@
         //daysLeft = 101;
         
         let changeDaysWord = (daysLeft) => {
-              var daysLeft = Math.abs(daysLeft);
-              var word = "дней",
-                day = "осталось";
-          
-              if(((daysLeft === 2) || (daysLeft % 10 === 2) || (daysLeft === 3) || (daysLeft % 10 === 3)  || (daysLeft === 4) || (daysLeft % 10 === 4))){
-                  word = "дня";
-              } 
-              if(daysLeft === 1 || daysLeft % 10 === 1){
+            let word = "дней",
+            day = "осталось";
+        
+            if(((daysLeft === 2) || (daysLeft % 10 === 2) || (daysLeft === 3) || (daysLeft % 10 === 3)  || (daysLeft === 4) || (daysLeft % 10 === 4))){
+                word = "дня";
+            } 
+            if(daysLeft === 1 || daysLeft % 10 === 1){
                 word = "день";
                 day =  "остался";
-              }
-              if((daysLeft === 11) || (daysLeft === 12) || (daysLeft === 13) || (daysLeft === 14) || (daysLeft % 100 === 11) || (daysLeft % 100 === 12) || (daysLeft % 100 === 13) | (daysLeft % 100 === 14) ){
+            }
+            if((daysLeft === 11) || (daysLeft === 12) || (daysLeft === 13) || (daysLeft === 14) || (daysLeft % 100 === 11) || (daysLeft % 100 === 12) || (daysLeft % 100 === 13) | (daysLeft % 100 === 14) ){
                 word = "дней";
-              }
-              if(daysLeft === 0 ){
+            }
+            if(daysLeft === 0 ){
                 return `С Новым годом!!!`;
-              }
+            }
 
-              return `До 2019 года ${day} ${daysLeft} ${word}.`;
+            return `До 2019 года ${day} ${daysLeft} ${word}.`;
         }
         
         p(todayDate, todayDateBlock);
         p(currentTime, clockBlock);
         p(changeDaysWord(daysLeft), daysLeftBlock);
     }
-
      setInterval(init,500);
 })();
