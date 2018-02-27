d634258a0a2c5dd62383f639e7d0df4050c83223
dima <dmitriy.dubchenko@gmail.com>
2018-02-27 02:20:22 +0200

    commit  27/02/18 , improve styles, ,logic(words form now is changing according to number of days)

--- js/main.js
+++ js/main.js
@@ -14,18 +14,41 @@
 
     let init = () => {
         let date = new Date(),
-        hours = date.getHours().length < 10 ? "0" + date.getHours() : date.getHours(),
-        minutes = date.getMinutes().length < 10 ? "0" + date.getMinutes() + "": date.getMinutes(),
+        hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours(),
+        minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() + "": date.getMinutes(),
         seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() + "": date.getSeconds(),
         todayDate = `Сегодня  ${weekdays[date.getDay()]},  ${date.getDate()} ${months[date.getMonth()]}`,
         currentTime = `Текущее время: ${hours}:${minutes}:${seconds}`,
         nextYear = Date.parse('2019-01-01T00:00:00')/(1000*60*60*24),
         currentYear = Date.parse(date)/(1000*60*60*24),
-        daysLeft = `До 2019 года осталось ${Math.floor(nextYear - currentYear)} дней.`;
+        daysLeft = Math.floor(nextYear - currentYear); 
+        //daysLeft = 101;
+        
+        let changeDaysWord = (daysLeft) => {
+              var daysLeft = Math.abs(daysLeft);
+              var word = "дней",
+                day = "осталось";
+          
+              if(((daysLeft === 2) || (daysLeft % 10 === 2) || (daysLeft === 3) || (daysLeft % 10 === 3)  || (daysLeft === 4) || (daysLeft % 10 === 4))){
+                  word = "дня";
+              } 
+              if(daysLeft === 1 || daysLeft % 10 === 1){
+                word = "день";
+                day =  "остался";
+              }
+              if((daysLeft === 11) || (daysLeft === 12) || (daysLeft === 13) || (daysLeft === 14) || (daysLeft % 100 === 11) || (daysLeft % 100 === 12) || (daysLeft % 100 === 13) | (daysLeft % 100 === 14) ){
+                word = "дней";
+              }
+              if(daysLeft === 0 ){
+                return `С Новым годом!!!`;
+              }
 
+              return `До 2019 года ${day} ${daysLeft} ${word}.`;
+        }
+        
         p(todayDate, todayDateBlock);
         p(currentTime, clockBlock);
-        p(daysLeft, daysLeftBlock);
+        p(changeDaysWord(daysLeft), daysLeftBlock);
     }
 
      setInterval(init,500);
